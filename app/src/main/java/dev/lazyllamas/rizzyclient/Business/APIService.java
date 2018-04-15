fb904eb0e40093cc79a1bef373d398d4a181ae27
artas182x <artas182x@gmail.com>
2018-04-15 11:07:13 +0200

    Changes

--- app/src/main/java/dev/lazyllamas/rizzyclient/Business/APIService.java
+++ app/src/main/java/dev/lazyllamas/rizzyclient/Business/APIService.java
@@ -4,14 +4,18 @@ import android.provider.ContactsContract;
 
 import java.util.ArrayList;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 
 public interface APIService {
@@ -36,6 +40,10 @@ public interface APIService {
 
     @POST("/profile/{id}")
     Call<ResponseBody> updateLocation(@Path("id") String userId, @Body Person person);
+
+    @Multipart
+    @POST("/profile/avatar/{id}")
+    Call<ResponseBody> uploadImage(@Part("image\"; filename=\"myfile.jpg\" ") RequestBody file);
 
 
 }
--- app/src/main/java/dev/lazyllamas/rizzyclient/MyProfile.java
+++ app/src/main/java/dev/lazyllamas/rizzyclient/MyProfile.java
@@ -3,10 +3,16 @@ package dev.lazyllamas.rizzyclient;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.CursorLoader;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
@@ -25,6 +31,12 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.Api;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.Format;
 import java.text.ParseException;
@@ -36,10 +48,14 @@ import dev.lazyllamas.rizzyclient.Business.APIService;
 import dev.lazyllamas.rizzyclient.Business.APIUtils;
 import dev.lazyllamas.rizzyclient.Business.Person;
 import dev.lazyllamas.rizzyclient.Business.Utils;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MyProfile extends AppCompatActivity {
 
@@ -52,6 +68,20 @@ public class MyProfile extends AppCompatActivity {
     CheckBox nordicwalking;
     private APIService mAPIService;
     private Person person;
+
+    public static final int GALLERY_REQUEST = 1;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK)
+            switch (requestCode){
+                case GALLERY_REQUEST:
+                    Uri selectedImage = data.getData();
+                    uploadFile(selectedImage);
+                    break;
+            }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +98,16 @@ public class MyProfile extends AppCompatActivity {
         skateboarding = findViewById(R.id.skateboardingCheckBox);
         cycling = findViewById(R.id.cyclingCheckBox);
         nordicwalking = findViewById(R.id.nordicwalkingCheckBox);
+
+        Button button_img = findViewById(R.id.button6);
+        button_img.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
+    }});
+
 
         Button button = findViewById(R.id.button4);
         button.setOnClickListener(new View.OnClickListener() {
@@ -215,5 +255,56 @@ public class MyProfile extends AppCompatActivity {
         });
 
 
+
+
     }
+
+    private String getRealPathFromURI(Uri contentUri) {
+        String[] proj = {MediaStore.Images.Media.DATA};
+        CursorLoader loader = new CursorLoader(this, contentUri, proj, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(column_index);
+        cursor.close();
+        return result;
+    }
+
+    private void uploadFile(Uri fileUri) {
+
+        //creating a file
+        File file = new File(getRealPathFromURI(fileUri));
+
+        //creating request body for file
+        RequestBody requestFile = RequestBody.create(MediaType.parse(getContentResolver().getType(fileUri)), file);
+
+        //The gson builder
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+
+        APIService apiservice = APIUtils.getAPIService();
+
+
+        Call<ResponseBody> call = apiservice.uploadImage(requestFile);
+
+
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "File Uploaded Successfully...", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Some error occurred...", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
 }
